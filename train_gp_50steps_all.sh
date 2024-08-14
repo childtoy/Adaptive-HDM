836213a908a531229a9c4ccff043e562d21f4ef1
Seulbi Lee <90444862+drizzle0171@users.noreply.github.com>
2024-08-14 17:37:48 +0900

    Delete train_gp_50steps_all.sh

--- train_gp_50steps_all.sh
+++
@@ -1,9 +0,0 @@
-CUDA_VISIBLE_DIVICES=0 python3 -m train.train_GPmotion \
-    --save_dir save/all_noise_blend \
-    --overwrite \
-    --corr_noise \
-    --dataset humanml \
-    --eval_during_training \
-    --diffusion_steps 50 \
-    --corr_mode all \
-    --noise_blend True \
